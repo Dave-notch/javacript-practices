8b5070545cc60f27c0ee514543e966d9b1c25cec
dave-notch <stsaamir86@gmail.com>
2025-10-30 14:51:21 +0300

    learned objects in javascript

---
+++ functions/functions.js
@@ -0,0 +1,31 @@
+// function checkVotingage(age){
+//     if(age>=18)
+//         console.log('youre eligible to vote')
+//     else if (age<18)
+//         console.log('youre not eligible to vote')
+// }
+// let user1=prompt('enter your age')
+// let user2=prompt('enter your age')
+// let user3=prompt('enter your age')
+// checkVotingage(user1,user2,user3)
+
+
+// function checkEvenodd(limit){
+//     for(i=1;i<limit;i++){
+//         if(i%2==0)
+//             console.log(`${i} is even`)
+//         else{
+//             console.log(`${i} is odd`)
+//         }
+//     }
+// }
+// r=prompt('enter a limit number')
+// checkEvenodd(r)
+
+// processPayment=callback=>callback();
+
+// setTimeout(processPayment(function(){
+//     console.log("payment successfull")
+// }),3000)
+
+setInterval(()=>console.log("hiii"),3000)
---
+++ objects/objects.js
@@ -0,0 +1,21 @@
+const products={
+    muuse: 'muuse',
+    'delivery-time': '1day',
+    //funtion inside object
+    // when funtion is inside object its called METHOD for example console.log 
+    fun: function funtionDemo(){
+        console.log("hii")
+    }
+}
+// dot notation is the defoult way to access inside objects 
+console.log(products.muuse)
+// we use [] when dot notation doesnt support such things like {- inside 'delivery-time' }
+console.log(products['delivery-time'])
+console.log(products.fun())
+
+console.log(JSON.stringify(products))
+
+const productsJSon=JSON.stringify(products)
+console.log(JSON.parse(productsJSon))
+
+
