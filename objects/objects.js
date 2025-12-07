2729bb5f197a9652d9ea222f4604c19b37f07ad6
Dave-notch <stsaamir86@gmail.com>
2025-12-07 10:45:43 +0300

    assingment done in object file

--- objects/objects.js
+++ objects/objects.js
@@ -32,6 +32,81 @@ let user=prompt('enter message property')
 c0nsole.l0g.push(user)
 console.log(c0nsole.l0g)
 
+// obove is self tought below is assingnment 
+
+const person = {
+  name: "Ayaan",
+  age: 21,
+  city: "Hargeisa"
+};
+console.log(person);
+
+const student = {
+  name: "Mahad",
+  math: 78,
+  science: 84,
+  english: 90
+};
+console.log(student.name, student.math);
+
+const phone = {
+  brand: "Tecno",
+  model: "Camon 20",
+  storage: "256GB"
+};
+console.log(phone.model);
+
+const account = {
+  owner: "Hodan",
+  balance: 1200,
+  currency: "USD"
+};
+console.log(account.balance);
+
+const menuItem = {
+  name: "Bariis iyo Hilib",
+  price: 5,
+  spicy: false
+};
+console.log(menuItem.name, menuItem.price);
+
+const car = {
+  make: "Toyota",
+  model: "Hilux",
+  year: 2019
+};
+console.log(car.year);
+
+const book = {
+  title: "Geeri iyo Nolosha",
+  author: "Cali Mataan",
+  pages: 210
+};
+console.log(book.title);
+
+const item = {
+  name: "Canjeero Pack",
+  quantity: 50,
+  price: 3
+};
+console.log(item.quantity);
+
+const player = {
+  name: "Abshir",
+  goals: 12,
+  assists: 7,
+  club: "Jeenyo"
+};
+console.log(player.goals);
+
+const weather = {
+  city: "Mogadishu",
+  temperature: 32,
+  condition: "Sunny"
+};
+console.log(weather.temperature, weather.condition);
+
+
 
 
 
